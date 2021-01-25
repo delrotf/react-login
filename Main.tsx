3295b73eae7a3679b38182dbce79b4a41694eded
delrotf <delrotf02@gmail.com>
2021-01-25 08:00:25 -0500

    1 file modified

--- Main.tsx
+++ Main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { LoginPage } from "./pages";
 import { Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { LoginContextProvider } from "./context";
 
 const Main = props => {
   return (
     <div>
-      <Route path="/" exact component={LoginPage} />
-      <Route path="/home" exact component={HomePage} />
+      <LoginContextProvider>
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/home" exact component={HomePage} />
+      </LoginContextProvider>
     </div>
   );
 };
